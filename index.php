13e1aac98709868e6614c7c945b780050d4e48d0
SerroTTi <serrotti@hotmail.com>
2017-04-12 14:13:10 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+          <head>
+                    <meta charset="UTF-8">
+                    <title>Tecnorio - Peças e Equipamentos Para Pintura</title>
+                    <meta name="description" content="Tecnorio Peças e Equipamentos Para Pintura"/>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                    <!--  <link rel="shortcut icon" href="img/............favicon"/>  -->
+                    <link rel="stylesheet" href="style.css"/>
+                    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+          </head>
+
+          <body>
+                    <header class="main_header container">
+                              <div class="content">
+                                        <a title="Tecnorio" alt="[Tecnorio]" href="./index.php"><img class="main_logo" title="Tecnorio" alt="[Tecnorio]" src="img/logo.png"/></a>
+                                        <h1 class="fontzero">Tecnorio - Peças e Equipamentos Para Pintura</h1>
+                                        <ul class="main_menu">
+                                                  <li><a href="./index.php" title="Tecnorio Inicial">Home</a></li>
+                                                  <li>|</li>
+                                                  <li><a href="./aempresa.php" title="Sobre a Tecnorio">A Empresa</a></li>
+                                                  <li>|</li>
+                                                  <li><a href="./produtos.php" title="Produtos da Tecnorio">Produtos</a></li>
+                                                  <li>|</li>
+                                                  <li><a href="./contato.php" title="Contato da Tecnorio">Contato</a></li>
+                                        </ul>
+                                        <div class="clear"></div>
+                              </div>
+                    </header>
+                    <!--FECHA CABEÇALHO-->
+
+                    <main class="container">
+                              <article class="container slider">
+                                        <div class="content">
+                                                  <header>
+                                                            <h1>Equipamento para Pintura Eletrostática <mark><b>TR 2000 M</b></mark></h1>
+                                                            <p class="tagline">O TR 2000 M Possui Display Simples e Seguro com Ajustes de Vazão, Transporte e Fluidização de Tinta Pó.</p>
+                                                  </header>
+                                                  <div class="clear"></div>
+                                        </div>
+                              </article>
+                              <!--FECHA SLIDER-->
+
+                              <section class="container destaque">
+                                        <div class="content">
+                                                  <header class="destaque_title">
+                                                            <h1>Tipos de Pintura</h1>
+                                                            <p class="tagline">Confira Alguns dos Produtos da Tecnorio</p>
+                                                  </header>
+                                                  <article class="destaque_item">
+                                                            <img title="Confira Equepamentos de Pintura" alt="[Confira Equepamentos de Pintura]" src="img/index-equip-pint.png"/>
+                                                            <h1>Equipamentos de Pintura</h1>
+                                                            <p>Equipamento para aplicação de tinta a pó.</p>
+                                                  </article>
+                                                  <article class="destaque_item">
+                                                            <img title="Confira Cabines de Pintura" alt="[Confira Cabines de Pintura]" src="img/index-cab-est.png"/>
+                                                            <h1>Cabines de Pintura</h1>
+                                                            <p>Cabine de pintura eletrostática a pó com sistema de recuperação.</p>
+                                                  </article>
+                                                  <article class="destaque_item">
+                                                            <img title="Confira Estufas de Cura" alt="[Confira Estufas de Cura]" src="img/index-estufa.png"/>
+                                                            <h1>Estufas de Cura</h1>
+                                                            <p>Estufas contínuas e estacionárias para cura de tintas e secagem.</p>
+                                                  </article>
+                                                  <article class="destaque_item">
+                                                            <img title="Confira os Queimadores" alt="[Confira os Queimadores]" src="img/index-estufa.fw.png"/>
+                                                            <h1>Queimadores</h1>
+                                                            <p>Queimadores a gás para aplicações em estufas.</p>
+                                                  </article>
+                                        </div>
+                                        <div class="clear"></div>
+                              </section>
+                              <!--FECHA DESTAQUE-->
+
+                              <section class="container destaque_2">
+                                        <div class="content">
+                                                  <article class="destaque_title">
+                                                            <h1>A Empresa</h1>
+                                                            <img title="Conheça a Empresa Tecnorio" alt="[Conheça a Empresa Tecnorio]" src="img/aempresahome.fw.png"/>
+                                                            <p>Constituída por profissionais que atuam com qualidade e eficiência técnica. Nosso principal objetivo é suprir as necessidades dos clientes que utilizam ou pretendem utilizar os recursos da Pintura Eletrostática a Pó e da Pintura Líquida em seus diversos setores de atuação.</p>
+                                                  </article>
+                                        </div>
+                                        <div class="clear"></div>
+                                        <div class="content">
+                                                  <article class="destaque_title">
+                                                            <h1>Contato</h1>
+                                                            <img title="Contate a Empresa Tecnorio" alt="[Contate a Empresa Tecnorio]" src="img/contatohome.jpg"/>
+                                                            <p>Trabalhamos com os mais diversos produtos na área de pintura em geral, tais como, Equipamentos de Pintura a Pó, Queimadores, Estufas, Cabines, Filtros, Exaustores, Pistolas de Sucção e Gravidade, Compressores, Peças de Reposição, entre outros.</p>
+                                                  </article>
+                                        </div>
+                                        <div class="clear"></div>
+                              </section>
+                              <!--FECHA DESTAQUE 2-->
+
+                    </main>
+                    <!--FECHA CONTEÚDO-->
+
+                    <footer>
+                              <h1>Pesquisas Sugeridas</h1>
+                              <nav>
+                                        <h1>Redes Sociais</h1>
+                                        <ul>
+                                                  <li><a target="_blank" title="Tecnorio Facebook" href="//www.facebook.com/TecnorioPecaseEquipamentosPintura">Facebook</a></li>
+                                                  <li><a target="_blank" title="Tecnorio Twitter" href="//twitter.com/tecnorio">Twitter</a></li>
+                                        </ul>
+                              </nav>
+                    </footer>
+                    <!--FECHA RODAPÉ-->
+
+          </body>
+</html>
